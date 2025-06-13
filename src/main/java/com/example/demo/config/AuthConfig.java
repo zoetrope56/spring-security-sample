e4bf82f474ab72b5ee35a77362630c87c628d4bf
tromm <chldpwl5620@gmail.com>
2025-06-13 16:25:39 +0900

    fix: 인증 등록 수정

--- src/main/java/com/example/demo/config/AuthConfig.java
+++ src/main/java/com/example/demo/config/AuthConfig.java
@@ -34,16 +34,18 @@ public class AuthConfig extends AbstractHttpConfigurer<AuthConfig, HttpSecurity>
 
     /**
      * 인증을 위한 필터 등록
+     *
      * @param http security http
      */
     @Override
     public void configure(HttpSecurity http) {
         val authenticationExceptionFilter = new AuthenticationExceptionFilter(this.objectMapper);
-//        val authenticationFilter = new AuthenticationFilter(this.jwtTokenProvider, this.redisTemplate, this.userMapper);
+        val authenticationFilter = new AuthenticationFilter(this.jwtTokenProvider, this.redisTemplate, this.userMapper, this.passwordEncoder);
         val authorizationFilter = new AuthorizationFilter(this.authenticationManager, this.jwtTokenProvider, this.redisTemplate, this.userMapper, this.passwordEncoder); // 인증 - 권한 관련 코드
 
         http.addFilterBefore(authenticationExceptionFilter, AuthenticationFilter.class)
-                .addFilterBefore(authorizationFilter, BasicAuthenticationFilter.class);
+                .addFilterBefore(authenticationFilter, BasicAuthenticationFilter.class)
+                .addFilterAt(authorizationFilter, BasicAuthenticationFilter.class);
 
     }
 
